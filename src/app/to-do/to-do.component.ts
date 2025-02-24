74c8b8955f36a35bce4ebbf1792ff265f7cd3311
Lindsay201291 <lindsay201291@gmail.com>
2025-02-24 15:10:53 -0400

    Se agrega funcionalidad para eliminar ítems en componente to-do

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { Task } from '../models/task.model';
 import { CommonModule } from '@angular/common';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 @Component({
   selector: 'app-to-do',
@@ -11,7 +12,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
   styleUrl: './to-do.component.css'
 })
 export class ToDoComponent {
-  id: number = 0;
+  id: number = 1;
   toDoItemDescription: string = '';
   toDoItemsList: Task[] = [];
 
@@ -23,26 +24,45 @@ export class ToDoComponent {
     let localToDoItems = localStorage.getItem('toDoItems');
     if (localToDoItems != null) {
       this.toDoItemsList = JSON.parse(localToDoItems);
-      this.id = this.toDoItemsList.length;
     }
   }
 
   AddItem() {
+    this.id = 1;
     if (this.toDoItemDescription.trim() !== "") {
-      this.id = this.id + 1;
-      const newTask = {
+      if (this.toDoItemsList.length > 0) {
+      let task = this.toDoItemsList.reduce((previous, current) => {
+        return current?.id > previous.id ? current : previous;
+      });
+
+      this.id = task.id + 1;
+      let newTask = {
         id: this.id,
         description: this.toDoItemDescription,
         status: false
       }
+
       this.toDoItemsList.push(newTask);
+      } else {
+        let newTask = {
+          id: this.id,
+          description: this.toDoItemDescription,
+          status: false
+        }
+        this.toDoItemsList.push(newTask);
+      }
     }
+
     localStorage.setItem('toDoItems', JSON.stringify(this.toDoItemsList));
     this.toDoItemDescription = '';
   }
 
-  changeStatus(i: number) {
-    console.log(i);
-      localStorage.setItem('toDoItems', JSON.stringify(this.toDoItemsList));
+  changeStatus() {
+    localStorage.setItem('toDoItems', JSON.stringify(this.toDoItemsList));
+  }
+
+  deleteItem(index: number) {
+    this.toDoItemsList.splice(index, 1);
+    localStorage.setItem('toDoItems', JSON.stringify(this.toDoItemsList));
   }
 }
